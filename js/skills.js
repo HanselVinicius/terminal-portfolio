1ad041c8c8636fe32cfe175e91444927ee1bdf17
Vinicius Hansel <hanelvinicius@gmail.com>
2024-05-15 14:08:40 -0300

    fix: not working redirect

--- js/skills.js
+++ js/skills.js
@@ -25,7 +25,7 @@ function pageCases(itemId){
             addUspParagraph(contentDiv,dropdown);
             break;
         case 'portfolio':
-            window.location.href = '/index.html';
+            window.location.href = 'terminal-portfolio/skills.html';
             break;
     }
 }
